382ed69621553ab50b71dde84c8d507c4f700382
Evan Clough <clough.evan@gmail.com>
2015-08-28 09:39:22 -0700

    Change layout of layout_page_spec code. Function same.

--- spec/layout_page_spec.rb
+++ spec/layout_page_spec.rb
@@ -19,13 +19,6 @@ describe 'the layout view' do
       expect(page).to have_content 'The Talking Tacos'
     end
 
-    it 'allows the user to add a new venue from the nav list' do
-      visit '/'
-      fill_in 'new_venue_name', with: 'the taco truck'
-      click_button 'new_venue_submit'
-      expect(page).to have_content 'The Taco Truck'
-    end
-
     it 'allows the user to delete bands from the nav menu' do
       band = Band.create name: 'The Talking Tacos'
       visit '/'
@@ -36,6 +29,21 @@ describe 'the layout view' do
   end
 
   describe 'venue menu', {type: :feature} do
+
+    it 'shows all venues' do
+      venue1 = Venue.create name: 'The Taco Truck'
+      venue2 = Venue.create name: 'The Burrito Bowl'
+      visit '/'
+      expect(page).to have_content venue1.name
+      expect(page).to have_content venue2.name
+    end
+
+    it 'allows the user to add a venue from the nav list' do
+      visit '/'
+      fill_in 'new_venue_name', with: 'The Taco Truck'
+      click_button 'new_venue_submit'
+      expect(page).to have_content 'The Taco Truck'
+    end
 
     it 'allows the user to delete venues from the nav menu' do
       venue = Venue.create name: 'The Taco Truck'
