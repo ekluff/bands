f916ca7626a991166f9ed3685014853167ad47d0
Evan Clough <clough.evan@gmail.com>
2015-08-28 08:35:11 -0700

    Write integration spec for layout view. It will test that the page displays a list of all bands in the band nav menu.

--- spec/layout_page_spec.rb
+++ spec/layout_page_spec.rb
@@ -1,1 +1,13 @@
 require 'spec_helper'
+
+describe 'the layout view' do
+  describe 'band menu', {type: :feature} do
+    it 'shows all bands' do
+      band1 = Band.create name: 'the talking tacos'
+      band2 = Band.create name: 'the merriful mariachis'
+      visit '/'
+      expect(page).to have_content band1.name
+      expect(page).to have_content band2.name
+    end
+  end
+end
