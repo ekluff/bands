497cebd8e4a9501032818a7b2dabef54dbd075e4
Evan Clough <clough.evan@gmail.com>
2015-08-28 08:23:52 -0700

    add root route to app.rb

--- app.rb
+++ app.rb
@@ -2,3 +2,10 @@ require 'bundler/setup'
 Bundler.require :default
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get '/' do
+  @bands = Band.all
+  @venues = Venue.all
+
+  erb :index
+end
